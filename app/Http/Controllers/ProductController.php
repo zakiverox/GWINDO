adeea7e324d56676ed29eae7f917ada539b1ee28
zakiverox <zakiverox@gmail.com>
2019-02-12 02:58:57 +0700

    step 1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -129,7 +129,7 @@ class ProductController extends ApiController
 
     /**
      * Display the specified resource.
-     *ssssss
+     *sssssssfdsfcdf
      * @param  \App\cr  $cr
      * @return \Illuminate\Http\Response
      */
